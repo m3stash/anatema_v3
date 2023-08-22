e455a09a79525c0bb6976f900d2f96b3919a166a
laurent <laurent.duchaussoy59@gmail.com>
2023-08-22 22:50:20 +0200

    feat: next refacto

--- Assets/scripts/World_Generation/Dungeon/DungeonUtils.cs
+++ Assets/scripts/World_Generation/Dungeon/DungeonUtils.cs
@@ -5,5 +5,23 @@ namespace DungeonNs {
         public static bool CheckIsOutOfBound(Vector2Int vector, int floorplanBound) {
             return vector.x < 0 || vector.x > floorplanBound || vector.y > floorplanBound || vector.y < 0;
         }
+
+        public static Vector2Int GetOffsetForDirection(DirectionalEnum direction) {
+            switch (direction) {
+                case DirectionalEnum.T:
+                return Vector2Int.up;
+                case DirectionalEnum.B:
+                return Vector2Int.down;
+                case DirectionalEnum.L:
+                return Vector2Int.left;
+                case DirectionalEnum.R:
+                return Vector2Int.right;
+                default:
+                return Vector2Int.zero;
+            }
+        }
+
     }
+
+
 }
